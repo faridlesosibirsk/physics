486cf131c976611ec40507ee8abb11d7730a6242
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2019-01-14 00:04:18 +0700

    factory

--- Project1.dpr
+++ Project1.dpr
@@ -2,13 +2,17 @@ program Project1;
 
 uses
   Vcl.Forms,
-  Unit1 in 'Unit1.pas' {Form1};
+  Unit1 in 'Unit1.pas' {Form1, Button1};
 
 {$R *.res}
+
+var
+  Form1: TForm1;
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
   Application.Run;
+
 end.
--- Unit1.pas
+++ Unit1.pas
@@ -1,61 +1,53 @@
 unit Unit1;
-
+
 interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
-  IniFiles;
+  Vcl.Controls, Vcl.Forms,
+  Vcl.StdCtrls;
 
 type
-  TForm1 = class(TForm)
-    procedure FormCreate(Sender: TObject);
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+
+  Factory = class
   private
-    { Private declarations }
   public
-    { Public declarations }
+    function ButtonCreate(AOwner: TForm): TButton;
+  published
+    constructor create; virtual; abstract;
   end;
 
-var
-  Form1: TForm1;
+  TForm1 = class(TForm)
+  private
+    Button1: TComponent;
+    Factory1: Factory;
+  published
+    procedure FormCreate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  end;
 
 implementation
 
 {$R *.dfm}
 
 procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
-var
-  Ini: TIniFile;
 begin
-  Ini := TIniFile.Create(ChangeFileExt(Application.ExeName, '.INI'));
-  try
-    Ini.WriteInteger('Form', 'Top', Top);
-    Ini.WriteInteger('Form', 'Left', Left);
-    Ini.WriteString('Form', 'Caption', caption);
-    Ini.WriteBool('Form', 'InitMax', WindowState = wsMaximized);
-  finally
-    Ini.Free;
-  end;
+  Button1.free;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-  Ini: TIniFile;
 begin
-  Ini := TIniFile.Create(ChangeFileExt(Application.ExeName, '.INI'));
-  try
-    Top := Ini.ReadInteger('Form', 'Top', 100);
-    Left := Ini.ReadInteger('Form', 'Left', 100);
-    caption := Ini.ReadString('Form', 'Caption', 'New Form');
-    if Ini.ReadBool('Form', 'InitMax', False) then
-      WindowState := wsMaximized
-    else
-      WindowState := wsNormal;
-  finally
-    Ini.Free;
-  end;
+  Button1 := Factory1.ButtonCreate(self);
+end;
+
+{ Factory }
+
+function Factory.ButtonCreate(AOwner: TForm): TButton;
+begin
+  result := TButton.create(AOwner);
+  result.Parent := AOwner;
 end;
 
 end.
+
