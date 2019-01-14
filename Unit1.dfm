e98ad9e7ce6a595675c5a329316a485e0c925448
Программист ИВЦ <farid@lfsibgtu.local>
2019-01-14 09:20:34 +0700

    Dictionary

--- Unit1.dfm
+++ Unit1.dfm
@@ -11,8 +11,25 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
-  OnClose = FormClose
   OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
+  object Button1: TButton
+    Left = 216
+    Top = 0
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 312
+    Top = 0
+    Width = 75
+    Height = 25
+    Caption = 'Button2'
+    TabOrder = 1
+    OnClick = Button2Click
+  end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -1,52 +1,99 @@
 unit Unit1;
-
-interface
+interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms,
-  Vcl.StdCtrls;
+  Vcl.StdCtrls, System.Generics.Collections, Vcl.Grids, Vcl.Outline,
+  Vcl.ComCtrls, Vcl.ExtCtrls;
 
 type
-
-  Factory = class
-  private
-  public
-    function ButtonCreate(AOwner: TForm): TButton;
-  published
-    constructor create; virtual; abstract;
-  end;
+  FactoryInterface = interface;
 
   TForm1 = class(TForm)
   private
-    Button1: TComponent;
-    Factory1: Factory;
+    /// <link>aggregation</link>
+    Factory1: FactoryInterface;
+  public
   published
+    Button1: TButton;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);  
     procedure FormCreate(Sender: TObject);
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  end;
+
+  FactoryInterface = interface
+    procedure ButtonCreate(AOwner: TForm);
+    procedure ButtonDestroy;
+  end;
+
+  Factory = class(TInterfacedObject, FactoryInterface)
+  private
+    ButtonDictionary: TDictionary<String, TButton>;
+  public
+    procedure ButtonCreate(AOwner: TForm);
+    procedure ButtonDestroy;
+  published
+    constructor create;
   end;
 
 implementation
 
 {$R *.dfm}
 
-procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
+procedure TForm1.Button1Click(Sender: TObject);
 begin
-  Button1.free;
+  Factory1.ButtonCreate(self);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Factory1.ButtonDestroy;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  Button1 := Factory1.ButtonCreate(self);
+  Factory1 := Factory.create;
 end;
 
 { Factory }
 
-function Factory.ButtonCreate(AOwner: TForm): TButton;
+procedure Factory.ButtonDestroy;
+var
+  b: TButton;
+  i:integer;
 begin
-  result := TButton.create(AOwner);
-  result.Parent := AOwner;
+  try
+    if (ButtonDictionary.ContainsKey('button1')) then
+    begin
+      ButtonDictionary['button1'].Parent := nil;
+      ButtonDictionary.Remove('button1');
+    end;
+  except
+  end;
+end;
+
+constructor Factory.create;
+begin
+  ButtonDictionary := TDictionary<String, TButton>.create;
+end;
+
+procedure Factory.ButtonCreate(AOwner: TForm);
+var
+  b: TButton;
+begin
+  try
+    if not(ButtonDictionary.ContainsKey('button1')) then
+    begin
+      b := TButton.create(nil);
+      b.Parent := AOwner;
+      b.Caption := 'button1';
+      ButtonDictionary.add('button1', b);
+    end;
+  except
+  end;
 end;
 
 end.
