3a8c0c674d12c6ca86b6d8ea43a44a514ca39424
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2019-01-30 22:25:40 +0700

    theory next back

--- TheoryLab1Unit.pas
+++ TheoryLab1Unit.pas
@@ -1,8 +1,9 @@
 unit TheoryLab1Unit;
-
+
 interface
 
 uses
+  SysUtils {IntToStr} ,
   Vcl.ExtCtrls {TPanel} ,
   AccessConnectionUnit,
   ConnectionUnit,
@@ -14,9 +15,16 @@ uses
 type
   TheoryLab1 = class(Lab1)
   private
-    BackButton: TButton;
+    AOwner: TForm;
+    OpenButton: TButton;
     TheoryButton: TButton;
     panel: TPanel;
+    NextButton: TButton;
+    BackButton: TButton;
+    i: integer;
+    Count: integer;
+    procedure NextPanel(Sender: TObject);
+    procedure BackPanel(Sender: TObject);
   published
     constructor create(AOwner: TForm; Open, toLab1Test: TNotifyEvent);
     procedure clear; override;
@@ -29,9 +37,11 @@ implementation
 constructor TheoryLab1.create(AOwner: TForm; Open, toLab1Test: TNotifyEvent);
 begin
   Connection1 := AccessConnection.create;
+  Count := Connection1.TheoryLab1.Count;
+  self.AOwner := AOwner;
   AOwner.Caption := 'TheoryLab1';
-  BackButton := TButton.create(AOwner);
-  with BackButton do
+  OpenButton := TButton.create(AOwner);
+  with OpenButton do
   begin
     Parent := AOwner;
     Caption := '< Open';
@@ -45,23 +55,77 @@ begin
     Left := 200;
     OnClick := toLab1Test;
   end;
+
+  NextButton := TButton.create(AOwner);
+  with NextButton do
+  begin
+    Left := 300;
+    Top := 400;
+    Caption := 'Далее';
+    OnClick := NextPanel;
+  end;
+  BackButton := TButton.create(AOwner);
+  with BackButton do
+  begin
+    Left := 200;
+    Top := 400;
+    Caption := 'Назад';
+    OnClick := BackPanel;
+  end;
+  i := -1;
+  NextPanel(nil);
+end;
+
+procedure TheoryLab1.NextPanel(Sender: TObject);
+begin
   panel := TPanel.create(nil);
   with panel do
   begin
     Parent := AOwner;
-    // Caption := Connection1.theoryLab1.First;
-    Caption := Connection1.TheoryLab1.Items[1];
+    i := i + 1;
+    Caption := Connection1.TheoryLab1.Items[i];
     Left := 300;
     Top := 300;
+    if i < Count - 1 then
+      NextButton.Parent := AOwner
+    else
+      NextButton.Parent := nil;
+    if i > 0 then
+      BackButton.Parent := AOwner
+    else
+      BackButton.Parent := nil;
+  end;
+end;
+
+procedure TheoryLab1.BackPanel(Sender: TObject);
+begin
+  panel := TPanel.create(nil);
+  with panel do
+  begin
+    Parent := AOwner;
+    i := i - 1;
+    Caption := Connection1.TheoryLab1.Items[i];
+    Left := 300;
+    Top := 300;
+    if i < 1 then
+      BackButton.Parent := nil
+    else
+      BackButton.Parent := AOwner;
+    if i > Count - 2 then
+      NextButton.Parent := nil
+    else
+      NextButton.Parent := AOwner;
   end;
 end;
 
 procedure TheoryLab1.clear;
 begin
   inherited;
+  NextButton.Parent := nil;
   panel.Parent := nil;
-  BackButton.Parent := nil;
+  OpenButton.Parent := nil;
   TheoryButton.Parent := nil;
 end;
 
 end.
+
